b23f42a43bb0b6227fb0e4ebf71c5086fd6d45ee
Belzoni <patrickhcostello@gmail.com>
2017-02-27 17:37:30 +1100

    added run config file

---
+++ runShiny.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+runShinyServer() {
+	docker run --rm -p 80:3838 \
+	-v /home/ec2-user/testshiny/testShiny/:/srv/shiny-server/ \
+	-v /home/ec2-user/shinylog/:/var/log/shiny-server/ \
+	rocker/shiny
+}
